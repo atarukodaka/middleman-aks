e46c0c823a632310b08e33fa01d493dc39d98d79
Ataru Kodaka <ataru.kodaka@gmail.com>
2015-03-27 05:12:19 +0000

    add templates

--- lib/middleman-aks.rb
+++ lib/middleman-aks.rb
@@ -1,4 +1,7 @@
+require "middleman-core"
+require "middleman-aks/template"
 require "middleman-aks/version"
+
 
 ::Middleman::Extensions.register(:aks) do
   require 'middleman-aks/extension'
---
+++ lib/middleman-aks/template/source/index.html.md
@@ -0,0 +1,8 @@
+---
+title: Home
+date: 2015-3-24
+---
+
+## Welcome
+
+herehereh
