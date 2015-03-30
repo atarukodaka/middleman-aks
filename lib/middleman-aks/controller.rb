1ff869a1f245a2be113990c0c761fb3cc4c1bd92
Ataru Kodaka <ataru.kodaka@gmail.com>
2015-03-30 15:01:56 +0000

    reject 'sitetree' concept

--- lib/middleman-aks/controller.rb
+++ lib/middleman-aks/controller.rb
@@ -1,10 +1,8 @@
 require 'middleman-aks/article_container'
-require 'middleman-aks/site_tree'
+#require 'middleman-aks/site_tree'
 require 'middleman-aks/index_creator'
 require 'middleman-aks/archives'
 require 'middleman-aks/breadcrumbs'
-
-require 'tree'
 
 module Middleman
   module Aks
@@ -12,7 +10,8 @@ module Middleman
     # == Controller Class Description
     #
     class Controller
-      attr_reader :site_tree
+      include ERB::Util
+#      attr_reader :site_tree
       #
       #
       def initialize(app, ext)
@@ -30,10 +29,31 @@ module Middleman
         @app.logger.warn "run() needs to be called before using 'articles()'" if @processors.empty?
         @processors[:article_container].articles
       end
-
-      def sitetree
-        @processors[:site_tree]
+      def root
+        @app.sitemap.find_resource_by_path("/#{@app.index_file}")
       end
+      def path_for(path)
+        @app.sitemap.find_resource_by_path(path)
+      end
+      def paths
+        @app.sitemap.resources.map(&:path)
+      end
+      def render_children(page=nil)
+        page ||= root
+        @app.content_tag(:li) do
+          [
+           @app.link_to(h(page.data.title || File.split(page.path).first.split("/").last), page),
+           @app.content_tag(:ul) do 
+             page.children.sort {|a, b| a.children.try(:size) <=> b.children.try(:size)}.map do |child|
+               render_children(child)
+             end.join.html_safe
+           end
+          ].join.html_safe
+        end
+      end
+#      def sitetree
+#        @processors[:site_tree]
+#      end
 
       # @! 
       #
@@ -51,8 +71,8 @@ module Middleman
         @processors = {
           article_container: Middleman::Aks::ArticleContainer.new(@app, self),
           archives: Middleman::Aks::Archives.new(@app, self),
-          index_creator: Middleman::Aks::IndexCreator.new(@app, self),
-          site_tree: Middleman::Aks::SiteTree.new(@app, self)
+          index_creator: Middleman::Aks::IndexCreator.new(@app, self)
+#          site_tree: Middleman::Aks::SiteTree.new(@app, self)
         }
         @processors.each do |name, processor|
           @app.sitemap.register_resource_list_manipulator(name, processor)
---
+++ lib/middleman-aks/template/source/hobby/cooking.html.md
@@ -0,0 +1,6 @@
+---
+title: cooking
+---
+
+- humberg
+
